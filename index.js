7bcbfaa4cca71c00257da3d754e0b450769556f9
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-14 14:33:06 +0200

    Created new version capable of creating and deploying constitutions

---
+++ index.js
@@ -0,0 +1,212 @@
+const pskBuildPath = '../../psknode/bin/scripts/pskbuild.js';
+require('../../psknode/bundles/pskruntime');
+require('../../psknode/bundles/psknode');
+require('../../psknode/bundles/edfsBar');
+
+function createConstitutionFromSources(sources, outputFolder, options, callback) {
+    const child_process = require('child_process');
+    const path = require('path');
+    const fs = require('fs');
+
+    if (typeof sources === 'string') {
+        sources = [sources];
+    }
+
+    if (typeof options === 'function') {
+        callback = options;
+        options = {}
+    }
+
+    options = Object.assign({
+        constitutionName: 'constitution'
+    }, options);
+
+    let sourcesNames = [];
+    let sourcesPaths = [];
+
+    if (sources && sources.length && sources.length > 0) {
+        sourcesNames = sources.map(source => path.basename(source));
+        sourcesPaths = sources.map(source => path.dirname(source));
+    }
+
+    sourcesNames = sourcesNames.join(',');
+    sourcesPaths = sourcesPaths.join(',');
+
+    const projectMap = {
+        [options.constitutionName]: {"deps": sourcesNames, "autoLoad": true},
+    };
+
+    getTmpDir('PSK_DOMAIN-', (err, tmpFolder) => {
+        if (err) {
+            return callback(err);
+        }
+
+        const projectMapPath = path.join(tmpFolder, 'projectMap.json');
+        fs.writeFile(projectMapPath, JSON.stringify(projectMap), 'utf8', (err) => {
+            if (err) {
+                return callback(err);
+            }
+
+            child_process.exec(`node ${pskBuildPath} --projectMap=${projectMapPath} --source=${sourcesPaths} --output=${outputFolder}`, (err) => {
+                if (err) {
+                    return callback(err);
+                }
+
+                callback(undefined, path.join(outputFolder, `${options.constitutionName}.js`));
+
+                fs.rmdir(tmpFolder, {recursive: true}, (err) => {
+                    if (err) {
+                        console.warn(`Failed to delete temporary folder "${tmpFolder}"`);
+                    }
+                });
+            });
+        });
+    });
+}
+
+function deployConstitutionCSB(constitutionBundle, edfsURL, callback) {
+    const EDFS = require('edfs');
+    const brickStorageStrategyName = "http";
+
+    $$.securityContext = require("psk-security-context").createSecurityContext();
+    $$.brickTransportStrategiesRegistry.add(brickStorageStrategyName, EDFS.createHTTPBrickTransportStrategy(edfsURL));
+
+    const edfs = EDFS.attach(brickStorageStrategyName);
+
+    edfs.createCSB((err, constitutionCSB) => {
+        if (err) {
+            return callback(err);
+        }
+
+        addFilesToArchive(constitutionBundle, constitutionCSB, willReturnSeed(constitutionCSB, callback));
+    });
+}
+
+function deployConstitutionBar(constitutionBundle, callback) {
+    const EDFS = require('edfs');
+    const brickStorageStrategyName = "http";
+
+    const edfs = EDFS.attach(brickStorageStrategyName);
+    const constitutionBAR = edfs.createBar();
+
+    addFilesToArchive(constitutionBundle, constitutionBAR, willReturnSeed(constitutionBAR, callback));
+
+}
+
+function getConstitutionFilesFromBar(seed, callback) {
+    const EDFS = require('edfs');
+    const brickStorageStrategyName = "http";
+
+    const edfs = EDFS.attach(brickStorageStrategyName);
+    const constitutionBAR = edfs.loadBar(seed);
+
+    readConstitutionFrom(constitutionBAR, callback)
+}
+
+function ensureEnvironmentIsReady(edfsURL) {
+    const EDFS = require('edfs');
+    const brickStorageStrategyName = "http";
+
+    if (!$$.securityContext) {
+        $$.securityContext = require("psk-security-context").createSecurityContext();
+    }
+
+    const hasHttpStrategyRegistered = $$.brickTransportStrategiesRegistry.has(brickStorageStrategyName);
+
+    if (!hasHttpStrategyRegistered) {
+        $$.brickTransportStrategiesRegistry.add(brickStorageStrategyName, EDFS.createHTTPBrickTransportStrategy(edfsURL));
+    }
+}
+
+/****************************** UTILITY FUNCTIONS ******************************/
+
+function addFilesToArchive(files, archive, callback) {
+    const path = require('path');
+
+    if (typeof files === 'string') {
+        files = [files];
+    }
+
+    asyncReduce(files, __addFile, null, callback);
+
+    function __addFile(_, filePath, callback) {
+        archive.addFile(filePath, path.basename(filePath), callback);
+    }
+}
+
+function readConstitutionFrom(archive, callback) {
+
+    archive.listFiles('', (err, files) => {
+        if (err) {
+            return callback(err);
+        }
+
+        asyncReduce(files, __readFile, [], callback);
+    });
+
+
+    function __readFile(pastFilesContent, filePath, callback) {
+        archive.readFile(filePath, (err, fileContent) => {
+            if (err) {
+                return callback(err);
+            }
+
+            pastFilesContent.push(fileContent);
+            callback();
+        });
+    }
+}
+
+function willReturnSeed(archive, callback) {
+    return function (err) {
+        if (err) {
+            return callback(err);
+        }
+
+        const seed = archive.getSeed();
+        callback(undefined, seed);
+    }
+}
+
+/**
+ * Traverse an array and collects result from calling handler on each array of the element
+ * It's similar to Array.prototype.reduce but it's asynchronous
+ */
+function asyncReduce(array, handler, currentValue, callback) {
+    function __callNext(index = 0) {
+        if (index >= array.length) {
+            return callback(undefined, currentValue);
+        }
+
+        handler(currentValue, array[index], (err, newCurrentValue) => {
+            if (err) {
+                return callback(err);
+            }
+
+            if (newCurrentValue) {
+                currentValue = newCurrentValue;
+            }
+
+            __callNext(index + 1);
+        })
+    }
+
+    __callNext();
+}
+
+function getTmpDir(dirNamePrefix, callback) {
+    const path = require('path');
+    const os = require('os');
+    const fs = require('fs');
+
+    const tmpFolder = os.tmpdir();
+    fs.mkdtemp(path.join(tmpFolder, dirNamePrefix), callback);
+}
+
+module.exports = {
+    createConstitutionFromSources,
+    deployConstitutionBar,
+    deployConstitutionCSB,
+    ensureEnvironmentIsReady,
+    getConstitutionFilesFromBar
+};
